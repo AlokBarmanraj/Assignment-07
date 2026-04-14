032c51a105f0694901b9f85d21fbd8c156a4bc1c
alok <alokbarmanraj2021@gmail.com>
2026-04-14 17:42:20 +0600

    count card

--- src/components/CountCard.jsx
+++ src/components/CountCard.jsx
@@ -2,29 +2,29 @@ import React from "react";
 
 const CountCard = () => {
   return (
-    <div className="w-9/12 mx-auto grid grid-cols-4 text-center">
-      <div className="bg-white shadow-sm p-8">
+    <div className="w-9/12 mx-auto grid grid-cols-4 text-center gap-5 pt-10">
+      <div className="bg-white shadow-sm p-8 border border-gray-200 rounded-xl">
         <div>
-          <h3>10</h3>
-          <h4>Total Friends</h4>
+          <h3 className="font-bold text-4xl text-[#244D3F]">10</h3>
+          <h4 className="font-bold text-xl text-[#64748B]">Total Friends</h4>
         </div>
       </div>
-      <div className="bg-white shadow-xl">
+      <div className="bg-white shadow-sm p-8 border border-gray-200 rounded-xl">
         <div>
-          <h3>3</h3>
-          <h4>On Track</h4>
+          <h3 className="font-bold text-4xl text-[#244D3F]">3</h3>
+          <h4 className="font-bold text-xl text-[#64748B]">On Track</h4>
         </div>
       </div>
-      <div className="bg-white shadow-xl">
+      <div className="bg-white shadow-sm p-8 border border-gray-200 rounded-xl">
         <div>
-          <h3>6</h3>
-          <h4>Need Attention</h4>
+          <h3 className="font-bold text-4xl text-[#244D3F]">6</h3>
+          <h4 className="font-bold text-xl text-[#64748B]">Need Attention</h4>
         </div>
       </div>
-      <div className="bg-white shadow-xl">
+      <div className="bg-white shadow-sm p-8 border border-gray-200 rounded-xl">
         <div>
-          <h3>12</h3>
-          <h4>Interactions This Month</h4>
+          <h3 className="font-bold text-4xl text-[#244D3F]">12</h3>
+          <h4 className="font-bold text-xl text-[#64748B]">Interactions This Month</h4>
         </div>
       </div>
     </div>
