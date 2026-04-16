13dd6b0b887556fb6f7b5192314984bec38eb0ba
alok <alokbarmanraj2021@gmail.com>
2026-04-16 23:30:40 +0600

    responsive

--- src/app/friend/[friendId]/page.jsx
+++ src/app/friend/[friendId]/page.jsx
@@ -8,6 +8,7 @@ import { RiDeleteBin5Line } from "react-icons/ri";
 
 const FriendDetails = async ({ params }) => {
   const { friendId } = await params;
+
   const res = await fetch(
     "https://gist.githubusercontent.com/AlokBarmanraj/34f69c6eebc425dadf6fc914f9a67acc/raw/67b9259f5906a667e5a41a05c2347dd7855cffea/friends.json",
   );
@@ -35,96 +36,126 @@ const FriendDetails = async ({ params }) => {
     }
   };
   return (
-    <div>
-      <div className="grid grid-cols-5 grid-rows-7 gap-5 w-9/12 mx-auto mt-20">
-        <div className="row-span-4 col-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg p-8 text-center space-y-3 flex flex-col items-center justify-center">
+    <div className="w-full px-5 lg:w-9/12 mx-auto mt-10 lg:mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 lg:grid-rows-7 gap-5">
+        <div className="lg:row-span-4 lg:col-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg p-6 text-center flex flex-col items-center justify-center space-y-3">
           <Image
             src={picture}
             alt="friend image"
             width={100}
             height={100}
             className="rounded-full mx-auto block"
-          ></Image>
-          <h1 className="text-2xl font-bold">{name}</h1>
+          />
+          <h1 className="text-xl md:text-2xl font-bold">{name}</h1>
+
           <p
-            className={`mt-2 inline-block px-3 py-1 rounded-full text-sm ${getStatusBgColor(status)}`}
+            className={`px-3 py-1 rounded-full text-sm ${getStatusBgColor(status)}`}
           >
             {status}
           </p>
-          <div className="flex justify-center gap-2">
+
+          <div className="flex flex-wrap justify-center gap-2">
             {tags.map((tag, index) => (
               <p
                 key={index}
-                className="bg-[#CBFADB] px-2 rounded-full font-bold"
+                className="bg-[#CBFADB] px-2 rounded-full text-sm font-bold"
               >
                 {tag}
               </p>
             ))}
           </div>
-          <p className="font-bold text-[#64748B] italic">{`"${bio}"`}</p>
-          <p className="text-[#64748B] font-bold">Email: {email}</p>
+
+          <p className="text-sm md:text-base font-bold text-[#64748B] italic">
+            {`"${bio}"`}
+          </p>
+
+          <p className="text-sm text-[#64748B] font-bold">Email: {email}</p>
         </div>
-        <div className="row-span-2 col-span-1 bg-white border border-gray-200 shadow-2xl rounded-lg flex flex-col justify-center items-center text-center space-y-2">
-          <h2 className="text-[#244D3F] font-bold text-4xl">
+
+        <div className="lg:row-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg flex flex-col justify-center items-center p-5">
+          <h2 className="text-3xl md:text-4xl font-bold text-[#244D3F]">
             {days_since_contact}
           </h2>
-          <p className="font-bold text-xl text-[#64748B]">Days Since Contact</p>
-        </div>
-        <div className="row-span-2 col-span-1 bg-white border border-gray-200 shadow-2xl rounded-lg flex flex-col justify-center items-center text-center space-y-2">
-          <h2 className="text-[#244D3F] font-bold text-4xl">{goal}</h2>
-          <p className="font-bold text-xl text-[#64748B]">Goal (Days)</p>
-        </div>
-        <div className="row-span-2 col-span-1 bg-white border border-gray-200 shadow-2xl rounded-lg flex flex-col justify-center items-center text-center space-y-2">
-          <h2 className="text-[#244D3F] font-bold text-4xl">{next_due_date}</h2>
-          <p className="font-bold text-xl text-[#64748B]">Next Due</p>
-        </div>
-        <div className="row-span-2 col-span-3 bg-white border border-gray-200 shadow-2xl rounded-lg p-8 text-center space-y-6">
-          <div className="flex justify-between">
-            <p className="text-[#244D3F] font-bold text-3xl">
-              Relationship Goal
-            </p>
-            <button className="btn">Edit</button>
-          </div>
-          <p className="flex">
-            <span className="text-2xl text-[#64748B]">Connect every</span>
-            <span className="font-bold text-2xl">{goal} days</span>
+          <p className="text-sm md:text-lg font-bold text-[#64748B] text-center">
+            Days Since Contact
           </p>
         </div>
-        <div className="row-span-1 col-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg p-8 text-center space-y-2">
-          <h2 className="font-bold text-2xl flex items-center justify-center gap-2">
+
+        <div className="lg:row-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg flex flex-col justify-center items-center p-5">
+          <h2 className="text-3xl md:text-4xl font-bold text-[#244D3F]">
+            {goal}
+          </h2>
+          <p className="text-sm md:text-lg font-bold text-[#64748B]">
+            Goal (Days)
+          </p>
+        </div>
+
+        <div className="lg:row-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg flex flex-col justify-center items-center p-5">
+          <h2 className="text-2xl md:text-3xl font-bold text-[#244D3F] text-center">
+            {next_due_date}
+          </h2>
+          <p className="text-sm md:text-lg font-bold text-[#64748B]">
+            Next Due
+          </p>
+        </div>
+
+        <div className="lg:row-span-2 lg:col-span-3 bg-white border border-gray-200 shadow-2xl rounded-lg p-6 space-y-4">
+          <div className="flex flex-col md:flex-row justify-between items-center gap-3">
+            <p className="text-xl md:text-3xl font-bold text-[#244D3F]">
+              Relationship Goal
+            </p>
+            <button className="btn btn-sm">Edit</button>
+          </div>
+
+          <p className="text-center md:text-left text-lg md:text-2xl text-[#64748B]">
+            Connect every{" "}
+            <span className="font-bold text-black">{goal} days</span>
+          </p>
+        </div>
+
+        <div className="lg:col-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg p-5 text-center">
+          <h2 className="text-lg md:text-2xl font-bold flex items-center justify-center gap-2 cursor-pointer">
             <HiMiniBellSnooze />
             Snooze 2 weeks
           </h2>
         </div>
-        <div className="row-span-1 col-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg p-8 text-center space-y-2 order-7">
-          <h2 className="font-bold text-2xl flex items-center justify-center gap-2">
+
+        <div className="lg:col-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg p-5 text-center order-7">
+          <h2 className="text-lg md:text-2xl font-bold flex items-center justify-center gap-2 cursor-pointer">
             <FaArchive />
             Archive
           </h2>
         </div>
-        <div className="row-span-1 col-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg p-8 text-center space-y-2 order-8">
-          <h2 className="font-bold text-red-500 text-2xl flex items-center justify-center gap-2">
+
+        <div className="lg:col-span-2 bg-white border border-gray-200 shadow-2xl rounded-lg p-5 text-center order-8">
+          <h2 className="text-lg md:text-2xl font-bold text-red-500 flex items-center justify-center gap-2 cursor-pointer">
             <RiDeleteBin5Line />
             Delete
           </h2>
         </div>
-        <div className="row-span-3 col-span-3 bg-white border border-gray-200 shadow-2xl rounded-lg p-8 text-center space-y-2">
-          <h2 className="text-2xl text-left text-[#244D3F]">Quick Check-In</h2>
-          <div className="grid grid-cols-3 gap-3 mt-16">
-            <div className="bg-gray-100 border border-gray-200 shadow-2xl rounded-lg p-8 text-center ">
-              <h2 className="font-bold text-2xl flex flex-col items-center justify-center gap-2">
+
+        <div className="lg:row-span-3 lg:col-span-3 bg-white border border-gray-200 shadow-2xl rounded-lg p-6">
+          <h2 className="text-xl md:text-2xl text-[#244D3F] mb-6">
+            Quick Check-In
+          </h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div className="bg-gray-100 rounded-lg p-6 text-center">
+              <h2 className="font-bold text-xl flex flex-col items-center gap-2">
                 <IoCall />
                 Call
               </h2>
             </div>
-            <div className="bg-gray-100 border border-gray-200 shadow-2xl rounded-lg p-8 text-center ">
-              <h2 className="font-bold text-2xl flex flex-col items-center justify-center gap-2">
+
+            <div className="bg-gray-100 rounded-lg p-6 text-center">
+              <h2 className="font-bold text-xl flex flex-col items-center gap-2">
                 <IoMdText />
                 Text
               </h2>
             </div>
-            <div className="bg-gray-100 border border-gray-200 shadow-2xl rounded-lg p-8 text-center ">
-              <h2 className="font-bold text-2xl flex flex-col items-center justify-center gap-2">
+
+            <div className="bg-gray-100 rounded-lg p-6 text-center">
+              <h2 className="font-bold text-xl flex flex-col items-center gap-2">
                 <IoVideocam />
                 Video
               </h2>
@@ -137,14 +168,3 @@ const FriendDetails = async ({ params }) => {
 };
 
 export default FriendDetails;
-
-
-
-
-
-
-
-
-
-
-
--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const TimeLinePage = () => {
-    return (
-        <div>
-            <h1>this is time line page</h1>
-        </div>
-    );
+  return (
+    <div className="w-9/12 mx-auto mt-16">
+      <h1 className="font-bold text-6xl">Timeline</h1>
+      <div>
+        <select
+          defaultValue="Filter Timeline"
+          className="select border-gray-200 shadow-2xl"
+        >
+          <option disabled={true}>Filter Timeline</option>
+          <option>Call</option>
+          <option>Text</option>
+          <option>Video</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default TimeLinePage;
